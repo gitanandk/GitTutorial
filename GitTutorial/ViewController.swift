2f879d60dc44a7293b7be1ae5d4809ade29eddc9
Anand Kumar <hs4jha@gmail.com>
2019-01-05 11:36:29 +0530

    Added a label to Storyboard & printed in it in view controller

--- GitTutorial/ViewController.swift
+++ GitTutorial/ViewController.swift
@@ -10,22 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
-	let message = "Hello Git!"
+    let message = "Hello Git!"
+    
+	@IBOutlet weak var messageLabel: UILabel!
 	
 	override func viewDidLoad() {
-		super.viewDidLoad()
-		print (message)
-		print (reverse(text: "stressed"))
-		// Do any additional setup after loading the view, typically from a nib.
-	}
-	func reverse (text: String) -> String {
-		return String (text.reversed())
-	}
+        super.viewDidLoad()
+        print (message)
+        messageLabel.text = reverse(text: "stressed")
+		
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    func reverse (text: String) -> String {
+        return String (text.reversed())
+    }
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	}
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 
 
 }
